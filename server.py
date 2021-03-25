c92c94d89d316a4e9afc2daa3e7707e6b9033baf
Степан <stepantishhen@mail.ru>
2021-03-25 13:25:52 +0300

    adding to heroku

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import os
+
 from currency_converter import CurrencyConverter
 from flask import Flask, render_template, request, redirect
 from form import MainForm
@@ -32,4 +34,5 @@ def converting():
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
