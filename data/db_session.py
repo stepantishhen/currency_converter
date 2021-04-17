3ad7615eda735b2eca3d57538596359b397b41d5
Степан <stepantishhen@mail.ru>
2021-04-17 21:30:56 +0300

    add comment

--- data/db_session.py
+++ data/db_session.py
@@ -18,9 +18,8 @@ def global_init(db_file):
         raise Exception("Необходимо указать файл базы данных.")
 
     conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
-    print(f"Подключение к базе данных по адресу {conn_str}")
 
-    engine = sa.create_engine(conn_str, echo=True)
+    engine = sa.create_engine(conn_str, echo=False)
     __factory = orm.sessionmaker(bind=engine)
 
     from . import __all_models
